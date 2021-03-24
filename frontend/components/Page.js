8d4d9039156f9c041dff1ce3891512f1019fd34a
Andreas Amstutz <tullo@users.noreply.github.com>
2021-03-24 21:28:01 +0100

    Fix css var defenition and children prop-types

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,4 +1,4 @@
-import Proptypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
 
@@ -18,7 +18,7 @@ const GlobalStyles = createGlobalStyle`
     --grey: #3A3A3A;
     --gray: var(--grey);
     --lightGrey: #e1e1e1;
-    --lightGray: var(--lightGray);
+    --lightGray: var(--lightGrey);
     --offWhite: #ededed;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
@@ -42,7 +42,6 @@ const GlobalStyles = createGlobalStyle`
   button {
     font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-
 `;
 
 const InnerStyles = styled.div`
@@ -62,6 +61,6 @@ export default function Page({ children }) {
 }
 
 Page.propTypes = {
-  children: Proptypes.any,
-  //  children: Proptypes.arrayOf(Proptypes.node),
+  children: PropTypes.any,
+  //  children: PropTypes.arrayOf(Proptypes.node),
 };
